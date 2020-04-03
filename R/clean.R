0c756109437e22b9ca42ecd550c917b425fcdc31
Ari Dyckovsky <aridyckovsky@gmail.com>
2020-04-03 14:15:09 -0600

    first commit

---
+++ R/clean.R
@@ -0,0 +1,14 @@
+#' Clean Function
+#'
+#' \code{clean} scrubs a sequenctial list of Qualtrics data export files
+#' detailing participant category responses to images within a directory of
+#' image blocks.
+#'
+#' @param x The variable x is something to be clean.
+#'
+#' @examples
+#'
+#' @export
+clean <- function(x) {
+	return(x)
+}
