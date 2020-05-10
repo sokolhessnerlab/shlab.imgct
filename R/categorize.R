665d1feafc65e7ea8b8952fee21391fdb21ce09a
Ari Dyckovsky <aridyckovsky@gmail.com>
2020-05-10 00:31:04 -0600

    reorder columns after categorization and save to file

--- R/categorize.R
+++ R/categorize.R
@@ -13,18 +13,22 @@
 #' @export
 categorize <- function(path) {
 
-  path_valid = file.path(path, "valid")
-  path_categorized = file.path(path, "categorized")
-  path_keys = file.path(path, "keys")
+  path_to_valid = file.path(path, "valid")
+  path_to_categorized = file.path(path, "categorized")
+  path_to_keys = file.path(path, "keys")
   
   c_key <- shlab.imgct::load_txt_key(
-    file.path(path_keys, "categorization_key.txt"), 
+    file.path(path_to_keys, "categorization_key.txt"), 
     key_template = "CATEGORIZATION"
   )
-  c_keys_by_name = names(c_key)
+  c_keys_by_code <- names(c_key)
+
+  #TODO: generalize the throwing out edge case values like "1,2" with a na_if
+  #statement or a replace statement somewhere, which only keeps the actual
+  #category numbers 1, 2, 3, 4, 5...but does this regardless of the task
 
   filenames <- list.files(
-    path = path_valid, 
+    path = path_to_valid, 
     pattern = "*.tsv",
     full.names = FALSE # maintain full path/to/file string
   )
@@ -35,20 +39,21 @@ categorize <- function(path) {
   for (fn in filenames) {
 
   	block <- shlab.imgct::load_block(
-  		file.path(path_valid, fn),
+  		file.path(path_to_valid, fn),
   		block_template = "VALID"
   	)
 
 		# pivot_longer and group by image_id to count category_id choices per image
 		block <- block %>%
 			tidyr::pivot_longer(
-				cols = -c("ip_address", "count_valid"), 
+				cols = -c("participantCode", "count_valid"), 
 				names_to = "image_id", 
 				values_to = "category_id", 
 				values_drop_na = FALSE # ...maybe
 			) %>%
 			dplyr::group_by(image_id) %>%
-			dplyr::count(category_id)
+			dplyr::count(category_id) %>%
+      dplyr::mutate(category_id = dplyr::na_if(category_id, "1,2"))
 
 		# pivot_wider to organize count of each category_id choice by image_id
 		block <- block %>% 
@@ -57,11 +62,13 @@ categorize <- function(path) {
 				names_from = category_id, 
 				values_from = n, 
 				values_fill = list(n = 0)
-			)
-
+			) %>%
+      dplyr::select(image_id, c_keys_by_code)
+      
 		# add convenience column with block index
-		block <- block %>%
-			dplyr::mutate(block_index = index)
+    #
+		# block <- block %>%
+		#	  dplyr::mutate(block_index = index)
 
 		df_list[[index]] <- block
 		index <- index + 1
@@ -74,6 +81,13 @@ categorize <- function(path) {
 
 	# Now bind rows of df_list
 	df_bind <- dplyr::bind_rows(df_list)
+
+  readr::write_tsv(
+    df_bind,
+    file.path(path_to_categorized, "categorized.tsv"),
+    append = FALSE,
+    col_names = TRUE
+  )
 
 	return(df_bind)
 
